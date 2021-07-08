ad520d1e8c6e3520501324dc99a6affd5097298a
Constantin <constantinu93@gmail.com>
2021-07-08 11:22:51 +0200

    feat: initialize project with gatsby

---
+++ README.md
@@ -0,0 +1,26 @@
+# MediaHub for ZOOM
+
+MediaHub for ZOOM is a simple but highly effective tool for all upcoming presentations via zoom:
+
+You simply log in, upload your media, assign titles and descriptions, and sort them in the order in which they will appear in your presentation. 
+
+In the next step, you link the MediaHub to the zoom meeting via the meeting ID.
+
+I created this project as Challenge for the admission process at CODE Berlin.
+
+## Summary
+
+- [Getting Started](#getting-started)
+- [License](#license)
+
+## Getting started
+
+Do the following to get started:
+
+- install project dependencies with `npm install`.
+- start the development server with `gatsby develop`
+
+## License
+
+MIT License - see the [LICENSE.md](LICENSE.md) file for
+details
---
+++ gatsby-config.js
@@ -0,0 +1,68 @@
+require(`dotenv`).config()
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const googleAnalyticsTrackingId = process.env.GOOGLE_ANALYTICS_ID
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `MediaHub`,
+    siteTitleAlt: `MediaHub - for Zoom`,
+    siteHeadline: `MediaHub - for Zoom`,
+    siteUrl: `http://localhost:8000/`,
+    siteDescription: `Easy and powerful presentation tool for zoom`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `conuko`,
+  },
+  flags: {
+    FAST_DEV: true,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    googleAnalyticsTrackingId && {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Cara - @lekoarts/gatsby-theme-cara`,
+        short_name: `Cara`,
+        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
